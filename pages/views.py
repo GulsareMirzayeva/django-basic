a01ebbdf3e1b12a85d69a35bfc4e007ea69aa3c2
GulsareMirzayeva <mirzayevagulsare@gmail.com>
2023-11-16 20:06:45 +0400

    first commit

---
+++ pages/views.py
@@ -0,0 +1,24 @@
+# from django.shortcuts import render
+
+# def index(request):
+#     data = ''
+#     ad = ''
+#     if request.method == 'POST':
+#         ad = request.POST["a1"]
+        
+#         if 'a1' in request.POST:
+#             data = 'sizin vezifeniz  ' +request.POST["a1"]
+   
+#     context = {
+
+#             'melumat': data,
+#              'ad'  :ad
+            
+#         }
+
+#     return render(request, 'index.html', context)
+
+
+#  if request.method == 'POST':
+#         data = len(request.POST['a'].split())
+
